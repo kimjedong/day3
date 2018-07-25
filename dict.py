c9cf9e3b19cf1afe3a62e0179637b24a0e68e68d
kimjedong <kimjedong@gmail.com>
2018-07-25 17:47:31 +0900

    딕트

---
+++ dict.py
@@ -0,0 +1,8 @@
+person = { 'name':'Kimjedong','address':'cheonan','email':'kimjedong@gmail.com'}
+#print(person)
+#print(person['name'])
+#print(person['email'])
+#print(person.items())
+for key, value in person.items():
+    print("\nKey \ "+"\""+ key+"'")
+    print("Value \ "+"'" +value+ "'")
