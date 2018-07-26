4a33c4976e6ecea61ec7b4a6cefbcec387d37400
kimjedong <kimjedong@gmail.com>
2018-07-26 16:11:53 +0900

    계산기,모듈,로또번호 추천기 추가

---
+++ ex1.py
@@ -0,0 +1,22 @@
+#import calculator as cal
+from calculator import *
+
+# num1=cal.add(1,2)
+# print (num1)
+# num2=cal.sub(1,2)
+# print (num2)
+# num3=cal.mul(1,2)
+# print (num3)
+# num4=cal.div(1,2)
+# print (num4)
+
+num1=add(1,2)
+print (num1)
+num2=sub(1,2)
+print (num2)
+num3=mul(1,2)
+print (num3)
+num4=div(1,2)
+print (num4)
+
+
---
+++ ex2.py
@@ -0,0 +1,6 @@
+from calculator import add,sub
+
+result = add(1,2)
+print(result)
+result = sub(3-1)
+print(result)
