1470a204cd93c93de6997a12eca3835f4066e314
kimjedong <kimjedong@gmail.com>
2018-07-26 14:29:05 +0900

    함수 매개변수와 return

--- function.py
+++ function.py
@@ -1,6 +1,15 @@
-def hello(name):
+def hello(name='', age='10'):
     print("Hello,"+name)
+    print(age + "years old")
 
-typing= input ("What is your name")
-hello(typing)
+#typing= input ("What is your name")
+
+#hello(typing)
 #print(name)
+
+name = input("Your name:")
+age =input("How old are you?")
+#hello(name,age)
+#hello(name)
+#hello()
+hello(age='30', name ='kkk')
---
+++ function2.py
@@ -0,0 +1,14 @@
+# def sum(num1,num2):
+#     return num1+num2
+
+# num1 = 10
+# num2 = 20
+
+# print(sum(num1,num2))
+
+def cart(fruits):
+    print("장바구니 안에 다음과 같은 과일이 있습니다")
+    for fruit in fruits:
+        print(fruit)
+fruits = ['apple', 'banana', 'oragne']
+cart(fruits)
